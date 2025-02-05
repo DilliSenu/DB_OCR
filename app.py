88295af558f6fda9e806ebd530768649c375e865
DilliBabuS <dillibabu101093@yahoo.com>
2025-02-05 23:01:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from PIL import Image
-import pytesseract
+# import pytesseract
 # import easyocr
 # from paddleocr import PaddleOCR
-from pytesseract import Output
+# from pytesseract import Output
 from process_file import *
 # import cv2
-# import google.generativeai as genai
+import google.generativeai as genai
 
 st.header("DB - OCR's")
 st.text("Upload an image to extract text")
@@ -44,12 +44,12 @@ if file:
     
     if st.button("Extract Text"):
         with st.status("Extracting Text"):
-            tess_output = pytesseract.image_to_data(image=image,output_type=Output.DICT)
-            tess_text, tess_conf = udf_tess_text(tess_output)
+            # tess_output = pytesseract.image_to_data(image=image,output_type=Output.DICT)
+            # tess_text, tess_conf = udf_tess_text(tess_output)
             # paddle = PaddleOCR(use_angle_cls=True,use_gpu=False, lang='en')
             # reader = easyocr.Reader(['en'])
-            # genai.configure(api_key="AIzaSyCo-j_zOU3NylZGvEHhnwYwEyGzVV8RMj0")
-            # gemini_model = genai.GenerativeModel(model_name='gemini-1.5-pro-exp-0801',generation_config=genai.GenerationConfig(temperature=0, top_p=1,top_k=0))
+            genai.configure(api_key="AIzaSyCo-j_zOU3NylZGvEHhnwYwEyGzVV8RMj0")
+            gemini_model = genai.GenerativeModel(model_name='gemini-1.5-pro-exp-0801',generation_config=genai.GenerationConfig(temperature=0, top_p=1,top_k=0))
 
             # for image in image_paths:
             #     input_image = cv2.imread(image)
@@ -75,22 +75,23 @@ if file:
         c3,c4,conf2 = st.columns([0.2,0.8,0.2])
         c5,c6,conf3 = st.columns([0.2,0.8,0.2])
         c7,c8,conf4 = st.columns([0.2,0.8,0.2])
-        with st.container():
-            c0.write("OCR")
-            c00.write("Extracted Text")
-            c000.write("Confidence Score")
-            c1.text("Tesseract")
-            c2.write(tess_text)
-            conf1.write(tess_conf)
-            # c3.text("Paddle")
-            # c4.write(paddle_text)
-            # conf2.write("NA")
-            # c5.text("Easy OCR")
-            # c6.write(easy_text)
-            # conf3.write(average_confidence)
-        st.text("Extraction Completed")
-
-        # response = gemini_model.generate_content([sample_file,"""Do OCR for the input document."""])
         # with st.container():
-        #     c7.text("Gemini LLM")
-        #     c8.write(response.text)
+        #     c0.write("OCR")
+        #     c00.write("Extracted Text")
+        #     c000.write("Confidence Score")
+        #     c1.text("Tesseract")
+        #     c2.write(tess_text)
+        #     conf1.write(tess_conf)
+        #     # c3.text("Paddle")
+        #     # c4.write(paddle_text)
+        #     # conf2.write("NA")
+        #     # c5.text("Easy OCR")
+        #     # c6.write(easy_text)
+        #     # conf3.write(average_confidence)
+        # st.text("Extraction Completed")
+
+        response = gemini_model.generate_content([file,"""Do OCR for the input document."""])
+        with st.container():
+            c7.text("Gemini LLM")
+            c8.write(response.text)
+        st.text("Extraction Completed")
